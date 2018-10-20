731d7637b9149b930254521eac9199e4fa6ece04
Caot22 <caot2204@gmail.com>
2018-10-20 13:58:34 -0500

    Creacion de proyecto servidor y clases del dominio en cliente
    Se crearon las clases de dominio sobre cuestionario en el cliente. En el servidor se crearon las entities para el acceso a datos

---
+++ qa-db.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE qa_db;
+USE qa_db;
+
+CREATE TABLE usuario (
+	nombre VARCHAR(150) PRIMARY KEY NOT NULL,
+	correo VARCHAR(150) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	foto_perfil BLOB) ENGINE=INNODB;
+
+CREATE TABLE cuestionario(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(150) NOT NULL,
+	cantidad_preguntas INT NOT NULL,
+	veces_jugado INT NOT NULL,
+	ultimo_ganador VARCHAR(150),
+	autor VARCHAR(150) NOT NULL,
+	FOREIGN KEY (autor) REFERENCES usuario(nombre) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=INNODB;
+
+CREATE TABLE pregunta(
+	id_cuestionario BIGINT NOT NULL,
+	numero INT NOT NULL,
+	descripcion VARCHAR(300),
+	imagen BLOB,
+	PRIMARY KEY (id_cuestionario, numero),
+	FOREIGN KEY (id_cuestionario) REFERENCES cuestionario(id) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=INNODB;
+
+CREATE TABLE respuesta(
+	id_cuestionario BIGINT NOT NULL,
+	numero_pregunta INT NOT NULL,
+	letra CHAR NOT NULL,
+	descripcion VARCHAR(300),
+	imagen BLOB,
+	correcta BOOLEAN NOT NULL,
+	PRIMARY KEY (id_cuestionario, numero_pregunta, letra)) ENGINE=INNODB;
